c17b956cccfbe671fe35563336a726dfb0346b9c
potof <slptmac@gmail.com>
2022-03-27 23:30:22 +0900

    update: color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,18 +1,17 @@
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { lightBlue } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#0288d1",
     },
-    secondary: {
-      main: "#19857b",
+    secondary: lightBlue,
+    background: {
+      default: "#EDF2F6",
     },
-    error: {
-      main: red.A400,
-    },
+    // text: { primary: "#ff9800" },
   },
 });
 
