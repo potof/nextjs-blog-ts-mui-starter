d647ccfd54daf90180fbe3a6dafe02ce9aa9c386
potof <slptmac@gmail.com>
2022-03-29 12:36:04 +0900

    add: twitter, github icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
 import { NextLinkComposed } from "../components/link";
 import config from "../site.config.json";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -12,49 +12,63 @@ const Header = () => {
           height: 60,
           backgroundColor: "white",
           boxShadow: "0 2px 4px #4385bb12",
-          marginBottom: 5,
+          pt: 1,
+          mb: 5,
           justifyContent: "space-between",
         }}
       >
         <Container maxWidth="md">
-          <Typography
-            component={NextLinkComposed}
-            to={{
-              pathname: "/",
-            }}
-            sx={{
-              color: "black",
-              fontSize: "22px",
-              fontWeight: "700",
-              textDecoration: "none",
-            }}
-          >
-            {"{{"} {config.title} {"}}"}
-          </Typography>
-
-          {config.twitter && (
-            <a href={config.twitter} target="_blank" rel="noopener noreferrer">
-              <TwitterIcon
-                fontSize="large"
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Typography
+                component={NextLinkComposed}
+                to={{
+                  pathname: "/",
+                }}
                 sx={{
                   color: "black",
+                  fontSize: "22px",
+                  fontWeight: "700",
                   textDecoration: "none",
                 }}
-              />
-            </a>
-          )}
+              >
+                {"{{"} {config.title} {"}}"}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} textAlign="right">
+              {config.twitter && (
+                <a
+                  href={config.twitter}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <TwitterIcon
+                    fontSize="large"
+                    sx={{
+                      color: "black",
+                      textDecoration: "none",
+                    }}
+                  />
+                </a>
+              )}
 
-          {config.github && (
-            <a href={config.github} target="_blank" rel="noopener noreferrer">
-              <GitHubIcon
-                fontSize="large"
-                sx={{
-                  color: "black",
-                  textDecoration: "none",
-                }}
-              />
-            </a>
-          )}
+              {config.github && (
+                <a
+                  href={config.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitHubIcon
+                    fontSize="large"
+                    sx={{
+                      color: "black",
+                      textDecoration: "none",
+                    }}
+                  />
+                </a>
+              )}
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     </>
